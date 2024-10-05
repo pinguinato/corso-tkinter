f8ed1c43b89564b045e0027c2fb4f7270408dffb
Roberto Gianotto <gianottoroberto@gmail.com>
2024-10-05 18:04:21 +0200

    banana survey wip

--- capitolo_1/banana_survey.py
+++ capitolo_1/banana_survey.py
@@ -22,17 +22,27 @@ title = tk.Label(
     fg='#FF0'
 )
 
-name_label = tk.Label(
-    root, 
-    text='What is your name', 
-)
-
+name_label = tk.Label(root, text='What is your name')
 # simple text input box
 name_inp = tk.Entry(root)
+# check button widget
+eater_inp = tk.Checkbutton(root, text='Check this box if you eat bananas')
+num_label = tk.Label(root, text='How many bananas do you eat per day?')
+num_inp = tk.Spinbox(root, from_=0, to=1000, increment=1)
+color_label = tk.Label(root, text='What is the best color for a banana?')
+color_inp = tk.Listbox(root, height=1)
+color_choices = ('Any', 'Green', 'Green-Yellow', 'Yellow', 'Brown-Spotted', 'Black')
+
+for choice in color_choices:
+    color_inp.insert(tk.END, choice)
+
 
 #title.pack()
 #name_label.pack()
 #name_inp.pack()
 
 
+##################
+# EXECUTION ######
+##################
 root.mainloop()
