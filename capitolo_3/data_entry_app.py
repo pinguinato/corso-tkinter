d7b6d0bccf70e8706968649ea177ebfe2ed2113d
Roberto Gianotto <gianottoroberto@gmail.com>
2024-10-07 22:52:19 +0200

    Date, Time and Technician section wip Record Information section

--- capitolo_3/data_entry_app.py
+++ capitolo_3/data_entry_app.py
@@ -35,7 +35,34 @@ ttk.Label(root, text="ABQ Data Entry Application", font=("TkDefaultFont", 16)).g
 # Data record form ######
 #########################
 
+drf = ttk.Frame(root)
+drf.grid(padx=10, sticky=(tk.E + tk.W))
+drf.columnconfigure(0, weight=1)
 
+#################################################
+# Data record form - Record information section #
+#################################################
+
+r_info = ttk.LabelFrame(drf, text='Record Information')
+r_info.grid(sticky=(tk.W + tk.E))
+for i in range(3):
+    r_info.columnconfigure(i, weight=1)
+
+# Date
+variables['Date'] = tk.StringVar()
+ttk.Label(r_info, text='Date').grid(row=0, column=0)
+tk.Entry(r_info, textvariable=variables['Date']).grid(row=1, column=0, sticky=(tk.W + tk.E))
+
+# Time
+time_values = ['8:00', '12:00', '16:00', '20:00']
+variables['Time'] = tk.StringVar()
+ttk.Label(r_info, text='Time').grid(row=0, column=1)
+ttk.Combobox(r_info, textvariable=variables['Time'], values=time_values).grid(row=1, column=1, sticky=(tk.W + tk.E))
+
+# Technician
+variables['Technician'] = tk.StringVar()
+ttk.Label(r_info, text='Technician').grid(row=0, column=2)
+ttk.Entry(r_info, textvariable=variables['Technician']).grid(row=1, column=2, sticky=(tk.W + tk.E))
 
 # esecuzione
 root.mainloop()
