3304561b0bdc794a269f422dbf0327452a2376a3
Roberto Gianotto <gianottoroberto@gmail.com>
2024-10-21 22:16:27 +0200

    Programma della scala termometrica con le classi in Python e Tkinter

---
+++ capitolo_4/scala_termometrica_classe/scala_termometrica_con_classi.py
@@ -0,0 +1,22 @@
+"""
+    Esercizio della scala termometrica svolto con le classi e Tkinter in Python
+"""
+
+import tkinter as tk
+
+class ScalaTermometrica(tk.Frame):
+    """ Realizza un frame con Label, input text e button per la scala termometrica """
+    pass
+
+
+
+
+
+
+root = tk.Tk()
+
+scala_termometrica = ScalaTermometrica(root)
+
+
+
+root.mainloop()
