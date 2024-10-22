f88a8813cae3f487eff15fff61a98e9f13887a0d
Roberto Gianotto <gianottoroberto@gmail.com>
2024-10-22 20:27:20 +0200

    classi e sottoclassi con tkinter wip

--- capitolo_4/scala_termometrica_classe/scala_termometrica_con_classi.py
+++ capitolo_4/scala_termometrica_classe/scala_termometrica_con_classi.py
@@ -6,17 +6,22 @@ import tkinter as tk
 
 class ScalaTermometrica(tk.Frame):
     """ Realizza un frame con Label, input text e button per la scala termometrica """
-    pass
+    def __init__(self, parent, label, inp_temp, inp_args, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
 
+        # definizione dei widget coinvolti
 
+        self.input = inp_temp(self, **inp_args)
+        self.label = tk.Label(self, text=label, anchor='w')
 
-
+        self.columnconfigure(1, weight=1)
+        self.label.grid(sticky=tk.E + tk.W)
+        self.input.grid(row=0, column=1, sticky=tk.E + tk.W)
 
 
 root = tk.Tk()
 
-scala_termometrica = ScalaTermometrica(root)
-
-
+scala_termometrica = ScalaTermometrica(root, 'DEGREE FAHRENHEIT', tk.Entry, {'bg':'yellow'})
+scala_termometrica.grid()
 
 root.mainloop()
