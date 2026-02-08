52168e62cc5d861843ad60333011267fd032e058
Roberto Gianotto <gianottoroberto@gmail.com>
2026-02-08 17:02:31 +0100

    esempi semplici di simpledialog widgets

---
+++ capitolo_7/simpledialog_demo/simpledialog_1.py
@@ -0,0 +1,5 @@
+from tkinter import simpledialog as sd
+
+word = sd.askstring(
+    'Word', 'What is the word?'
+)
---
+++ capitolo_7/simpledialog_demo/simpledialog_2.py
@@ -0,0 +1,5 @@
+from tkinter import simpledialog as sd
+
+word = sd.askinteger(
+    'Times', 'How many bananas do you want?'
+)
